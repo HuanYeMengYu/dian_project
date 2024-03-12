77ff2cbf9eeaf2e5a30ff9948119d3e4ccd76e42
livingston <c2592038287@gmail.com>
2024-03-12 23:04:21 +0800

    set up project structure

---
+++ Player/src/player.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "video_decoder.h"
+
+int main(int argc, char* argv[])
+{
+    printf("初始值：optind = %d，opterr = %d\n", optind, opterr);
+    int opt;
+    while ((opt = getopt(argc, argv, "vha:b:c::")) != -1)
+    { 
+        printf("开始解析参数：optind = %d\n", optind);
+        switch (opt) 
+        {    
+                case 'v':
+                        printf("捕获到选项: -v\n");
+                        printf("系统版本v1.1.0\n");   
+                        break;
+                case 'h':
+                        printf("捕获到选项: -h\n"); 
+                        printf("用法示例：\n");
+                        break;
+        }
+    }
+    return 0;
+}
