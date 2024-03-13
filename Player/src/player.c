5a84386b4ccaff67e8c71ffa9e3aa346a6e704e2
livingston <c2592038287@gmail.com>
2024-03-13 14:44:10 +0800

    通过average pooling调整分辨率

--- Player/src/player.c
+++ Player/src/player.c
@@ -3,7 +3,50 @@
 #include <unistd.h>
 #include "../include/video_decoder.h"
 
-// 打印视频帧
+// 调整视频帧的分辨率
+void resize(Frame cur_frame, Frame* new_frame, int pool_size, int strides){
+    // 创建调整分辨率后的新视频帧
+    new_frame->width = (int)((cur_frame.width - pool_size)/(double)strides) + 1;
+    new_frame->height = (int)((cur_frame.height - pool_size)/(double)strides) + 1;
+    new_frame->linesize = new_frame->width * 3;
+    new_frame->data = (unsigned char*)malloc(new_frame->linesize*new_frame->height*sizeof(char));
+    // average pooling
+    for(int y=0;y<new_frame->height;y++){
+        for(int x=0;x<new_frame->width;x++){
+            int old_index = (y*cur_frame.width + x)*strides*3;
+            int new_index = (y*new_frame->width+x)*3;
+            unsigned int sum_r = 0, sum_g = 0, sum_b = 0;
+            for(int j=0;j<pool_size;j++){
+                for(int i=0;i<pool_size;i++){
+                    sum_r += cur_frame.data[old_index+(j*cur_frame.width+i)*3];
+                    sum_g += cur_frame.data[old_index+1+(j*cur_frame.width+i)*3];
+                    sum_b += cur_frame.data[old_index+2+(j*cur_frame.width+i)*3];
+                }
+            } 
+            new_frame->data[new_index] = (int)(sum_r/pool_size);
+            new_frame->data[new_index+1] = (int)(sum_g/pool_size);
+            new_frame->data[new_index+2] = (int)(sum_b/pool_size);
+        }
+    }
+    return;
+}
+
+// 初始化新视频帧
+void init_frame(Frame* new_frame){
+    new_frame->data = NULL;
+    new_frame->height = new_frame->linesize = new_frame->width = 0;
+    return;
+}
+
+// 销毁调整分辨率后的新视频帧
+void destroy_frame(Frame* new_frame){
+    free(new_frame->data);
+    new_frame->data = NULL;
+    new_frame->height = new_frame->linesize = new_frame->width = 0;
+    return;
+}
+
+// 打印视频帧(控制背景颜色，打印空字符)
 void print_frame(Frame cur_frame){
     for (int y = 0; y < cur_frame.height; ++y) {
         for (int x = 0; x < cur_frame.width; ++x) {
@@ -15,31 +58,36 @@ void print_frame(Frame cur_frame){
         }
         printf("\e[0m\n"); // 恢复默认背景色
     }
+    return;
 }
 
 // 打印视频
-void print_video(){
+void print_video(const char* filename, int pool_size, int strides){
     // 打开视频文件
-    int status = decoder_init("../ref_video/BadApple样例参考.mp4");
+    int status = decoder_init(filename);
     if(status == -1){
         printf("视频文件打开失败！\n");
         exit(-1);
-    } 
-    Frame cur_frame = decoder_get_frame();
+    }
+    Frame cur_frame;
+    for(int i=0;i<1000;i++)
+        cur_frame = decoder_get_frame();
     if(cur_frame.height==0 && cur_frame.linesize==0 && cur_frame.width==0 && cur_frame.data==NULL){
         printf("视频帧已全部获取！\n");
         decoder_close();
         return;
     }
-    print_frame(cur_frame);
+    Frame new_frame;
+    init_frame(&new_frame);
+    resize(cur_frame, &new_frame, pool_size, strides);
+    print_frame(new_frame);
+    destroy_frame(&new_frame);
     decoder_close();
     return;
 }
 
 int main(int argc, char* argv[])
 {
-    print_video();
-
     // 命令行参数解析
     printf("初始值：optind = %d，opterr = %d\n", optind, opterr);
     int opt;
@@ -58,6 +106,11 @@ int main(int argc, char* argv[])
                         break;
         }
     }
+
+    print_video("../ref_video/bad_apple.mp4", 1, 1);
+    print_video("../ref_video/BadApple样例参考.mp4", 2, 2);
+    print_video("../ref_video/dragon.mp4", 1, 1);
+    print_video("../ref_video/无牙仔跳舞样例参考.mp4", 2, 2);
     return 0;
 }
 
