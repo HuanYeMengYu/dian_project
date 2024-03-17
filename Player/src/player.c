63cdce97e3e4c6f5a9c392153a710283da606c99
livingston <c2592038287@gmail.com>
2024-03-17 20:31:14 +0800

    命令行长选项解析

--- Player/src/player.c
+++ Player/src/player.c
@@ -1,8 +1,19 @@
 #include "../include/video_decoder.h"
 #include <bits/getopt_core.h>
+#include <bits/getopt_ext.h>
+#include <sys/types.h>
 
 const char* version = "dian-player v1.0";
 const char* default_filename = "../ref_video/bad_apple.mp4";
+static struct option long_options[] =
+    {  
+        {"version", no_argument, NULL, 'v'},
+        {"help", no_argument, NULL, 'h'},
+        {"color", no_argument, NULL, 'c'},
+        {"resize", optional_argument, NULL, 'r'},
+        {"file", optional_argument, NULL, 'f'},
+        {NULL, 0, NULL, 0},
+    }; 
 
 int main(int argc, char* argv[])
 {
@@ -10,7 +21,8 @@ int main(int argc, char* argv[])
     int pool_size=1, strides=1;
     bool version_flag=false, help_flag=false, color_flag=false, resize_flag=false;
     int opt = -1;
-    while ((opt = getopt(argc, argv, "vhcr::f::")) != -1)
+    int option_index = 0;
+    while ((opt = getopt_long(argc, argv, "vhcr::f::", long_options, &option_index)) != -1)
     {
         switch (opt) 
         {   
