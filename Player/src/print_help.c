6c649b8c24564781a0644d478fe3d6a8e54bd64e
livingston <c2592038287@gmail.com>
2024-03-17 23:23:27 +0800

    补全help命令文档

--- Player/src/print_help.c
+++ Player/src/print_help.c
@@ -1,6 +1,10 @@
 #include "../include/video_decoder.h"
+#include <string.h>
+#include <unistd.h>
+
+const char* help = "Usage: ./player [options]\nOptions:\n  --version                Display program version.\n  --help                   Display the help manual.\n  --color                  Choose to play rgb-video or not.\n  --resize                 Resize the video's resolution.\n  --file                   Choose which video to play.\n  -v                       Display program version.\n  -h                       Display the help manual.\n  -c                       Choose to play rgb-video or not.\n  -r                       Resize the video's resolution.\n  -                        Choose which video to play.\n";
 
 void print_help(void){
-     
+    write(STDOUT_FILENO, help, strlen(help));
     return;
 }
